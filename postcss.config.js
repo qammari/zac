ace9a40b9e8ac5ef499fce0e8cbef97307d230cc
Mostapha Qammari <mostapha.qammari@zyda.com>
2021-11-05 14:03:47 +0200

    Linking tailwind.config.js to postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
+const tailwindcss = require('tailwindcss');
+
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss'),
+    tailwindcss('./tailwind.config.js'),
     require('autoprefixer')
   ]
 }
