046f0a4396e73d47ac37ec8e376c38bdd5775ffb
omar <omarmarzook92@gmail.com>
2021-11-08 23:48:48 +0200

    add lineHeight to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
+const tokens = require('./tokens.json');
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        lineHeight: () => {
+          const jsonLineHeights = tokens.global.lineHeights;
+          const lineHeightsKeys = Object.keys(tokens.global.lineHeights);
+          const obj = {};
+
+            lineHeightsKeys.forEach((lineHeight) => {
+                return (obj[
+                    `${jsonLineHeights[lineHeight]['value']}`
+                ] = `${jsonLineHeights[lineHeight]['value']}px`);
+            });
+
+            return obj;
+        },
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
