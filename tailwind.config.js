6a3bf60f24e5a84e7bd47dd07639dadc0ed05cbc
omar <omarmarzook92@gmail.com>
2021-11-08 23:54:12 +0200

    add font-sizes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,23 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        fontSize: () => {
+            const jsonFontSizes = tokens.global.fontSizes;
+            const fontSizesKeys = Object.keys(tokens.global.fontSizes);
+            const obj = {};
+
+            fontSizesKeys.forEach((fontSize) => {
+                return (obj[
+                    `${jsonFontSizes[fontSize]['value']}`
+                ] = `${jsonFontSizes[fontSize]['value']}px`);
+            });
+
+            return obj;
+        },
         lineHeight: () => {
-          const jsonLineHeights = tokens.global.lineHeights;
-          const lineHeightsKeys = Object.keys(tokens.global.lineHeights);
-          const obj = {};
+            const jsonLineHeights = tokens.global.lineHeights;
+            const lineHeightsKeys = Object.keys(tokens.global.lineHeights);
+            const obj = {};
 
             lineHeightsKeys.forEach((lineHeight) => {
                 return (obj[
